a5df6728abb0f667e01f3119a3468ca8c7b8bad1
Victor Polyakov <polyakov.victor21@gmail.com>
2023-07-07 12:26:32 +0300

    edit filter

--- django/rental_project/rental/urls.py
+++ django/rental_project/rental/urls.py
@@ -5,7 +5,6 @@ app_name = 'rental'
 
 urlpatterns = [
     path('', views.home_page_view, name='home'),
-    path('filter/', views.explore_listings, name='filter'),
     path('explore/', views.explore_page_view, name='explore'),
     path('host/', views.host_page_view, name='host'),
     path('product/<int:apartment_id>', views.product_page_view, name='apartment_details'),
--- django/rental_project/rental/views.py
+++ django/rental_project/rental/views.py
@@ -18,23 +18,26 @@ def product_page_view(request, apartment_id):
 
 
 def explore_page_view(request):
-    apartments = get_apartment_data()
+    max_price = request.GET.get("max_price")
+    title = request.GET.get("title")
+    country = request.GET.get("country")
+    city = request.GET.get("city")
+    property_type = request.GET.get("type")
+    room_count = request.GET.get("room_count")
+    variables = [
+        max_price,
+        title,
+        country,
+        city,
+        property_type,
+        room_count
+    ]
+    if any(variables):
+        apartments = get_filter_apartment(max_price, title, country, city, property_type, room_count)
+    else:
+        apartments = get_apartment_data()
     paginator = Paginator(apartments, 9)
     page_number = request.GET.get('page')
     apartment_data = paginator.get_page(page_number)
     context = {'apartment_data': apartment_data}
     return render(request, 'rental/explore.html', context)
-
-
-def explore_listings(request):
-    max_price = request.GET["max_price"]
-    title = request.GET["title"]
-    country = request.GET["country"]
-    city = request.GET["city"]
-    property_type = request.GET["type"]
-    room_count = request.GET["room_count"]
-    apartment_data = get_filter_apartment(max_price, title, country, city, property_type, room_count)
-
-    context = {"apartment_data": apartment_data}
-
-    return render(request, "rental/explore.html", context)
